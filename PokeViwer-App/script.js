e0fb834a67a1defc8f2863581d013d55ff2e5c21
Igor Gomes Leandro <131786409+igorgleandro@users.noreply.github.com>
2025-09-06 23:59:59 +0100

    Add information at the viewer

--- PokeViwer-App/script.js
+++ PokeViwer-App/script.js
@@ -31,9 +31,15 @@ async function renderPokemon(url) {
   pokeResults.forEach(pokemon => {
     const btn = document.createElement("button");
     btn.textContent = pokemon.name;
+    const pokeViwerURL = pokemon.url;
     btn.classList.add("select-pokemon-button")
     pokemonButtons.appendChild(btn);
     fetchPokeAPI();
+    
+    btn.addEventListener('click', (e) =>{
+        e.preventDefault();
+        renderPokemonViwer(pokeViwerURL); 
+        console.log(pokeViwerURL) });
   });
 
 //If have previous
@@ -70,3 +76,50 @@ async function renderPokemon(url) {
 
   //Initial Render
 renderPokemon();
+
+//Pokemon Viwer
+
+async function fetchViwerAPI(url) {
+  const api = url;
+  const response = await fetch(api);
+  const data = await response.json();
+  
+  return data; 
+}
+
+
+async function renderPokemonViwer(url) {
+  
+  const apiURL = url;
+  const pokemonViwer = document.getElementById("pokemonViwerBox");
+  const pokemonImgViwer = document.getElementById("pokemonImageBox");
+ 
+  // Clean Before Render
+    pokemonViwer.innerHTML = "";
+    pokemonImgViwer.innerHTML ="";
+    
+  const pokemons = await fetchViwerAPI(apiURL);
+
+  const pokeViwerResults = pokemons.name;
+  console.log(pokeViwerResults);
+
+  const divNumber = document.createElement("div");
+  divNumber.classList.add("viwer-heading");
+  divNumber.textContent = pokemons.order;
+  pokemonViwer.appendChild(divNumber);
+
+  const divName = document.createElement("div");
+  divName.classList.add("viwer-name-pokemon");
+  divName.textContent = pokemons.name;
+  pokemonViwer.appendChild(divName);
+
+  const divImg = document.createElement("img");
+  divImg.classList.add("viwer-name-pokemon");
+  divImg.src = pokemons.sprites.front_default;
+  pokemonViwer.appendChild(divImg);
+
+  //class="viwer-heading"
+  
+
+  
+}
